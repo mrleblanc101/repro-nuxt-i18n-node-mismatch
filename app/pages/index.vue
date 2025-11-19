e10f77e1ddb4db44a0ce3c81900b4b1c389cb102
Sébastien LeBlanc <mrsebastienleblanc@icloud.com>
2025-11-19 02:59:51 -0500

    wip

---
+++ app/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <img
+        class="quebec-city-logo"
+        src="@/assets/img/ville-quebec.svg?url"
+        :alt="t('Partenaires')"
+        width="276"
+        height="81"
+    />
+    <img
+        class="quebec-city-logo"
+        src="@/assets/img/ville-quebec.svg?url"
+        :alt="'Test'"
+        width="276"
+        height="81"
+    />
+  </div>
+</template>
+
+<script setup>
+const { t, locale } = useI18n();
+console.log(locale.value);
+</script>
+
+<style>
+html {
+    background-color: black;
+}
+</style>
+
+
+<i18n lang="json">
+{
+    "en": {
+        "Partenaires": "Partners"
+    }
+}
+</i18n>
